2a5708d6e985bdb2eee564958a94458fb75a3c81
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-12 21:31:09 -0300

    Feat: DB Connection Implemented

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose, { mongo } from 'mongoose';
 
 const app = express();
 
@@ -19,4 +20,9 @@ const server = http.createServer(app);
 
 server.listen(8080,() => {
   console.log('Server running on http://localhost/8080/')
-})
+});
+
+const MONGO_URL ='mongodb+srv://Luiz:11072001@users.wqlcjqw.mongodb.net/users?retryWrites=true&w=majority'; 
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
