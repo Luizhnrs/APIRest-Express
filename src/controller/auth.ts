fac62637fbb408a8f9e5df109ab77d246d9814dd
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-13 14:43:23 -0300

    Feat: Authentication Controller Done

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,9 +1,29 @@
 import express from 'express'
+
+import { getUserByEmail, createUser } from '../db/users';
+import { authentication, random } from '../support';
+
 export const register = async (req:express.Request, res: express.Response) => {
-  try{
-    const {email, password, username} = req.body;
-  }catch(error){
-    console.log(error);
-    return res.sendStatus(400);
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email);
+  
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+    
   }
-}
