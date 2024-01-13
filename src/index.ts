1602d387c0e861d07505f293176323690aada000
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-12 21:54:13 -0300

    Correction: Imports to Node16

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,33 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import mongoose, { mongo } from 'mongoose';
-import { error } from 'console';
+import mongoose, { mongo } from "mongoose";
+import { error } from "console";
+import http from "http";
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 
 const app = express();
 
-app.use(cors({
-  credentials: true,
-}));
- 
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
 app.use(compression());
-app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080,() => {
-  console.log('Server running on http://localhost/8080/')
+server.listen(8080, () => {
+  console.log("Server running on http://localhost/8080/");
 });
 
-const MONGO_URL ='mongodb+srv://Luiz:11072001@users.wqlcjqw.mongodb.net/users?retryWrites=true&w=majority'; 
+const MONGO_URL =
+  "mongodb+srv://Luiz:11072001@users.wqlcjqw.mongodb.net/users?retryWrites=true&w=majority";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
-mongoose.connection.on('error', (error: Error) => console.log(error))
+mongoose.connection.on("error", (error: Error) => console.log(error));
